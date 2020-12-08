ac55764e1fc536be827f546d3833c763ea931638
YehudahDavid <70154918+YehudahDavid@users.noreply.github.com>
2020-12-08 12:09:24 -0600

    Update Comp141.py

--- Comp141.py
+++ Comp141.py
@@ -1,37 +1,43 @@
 import random
 import string
 
-class dnaTools:
-    length = int(input('Strand Length: '))
-    nucleotides ={'A' : 'T', 'T' : 'A', 'C' : 'G', 'G' : 'C'}
-    def randStr(length):
-        template = "ATGC"
-        tempList = []
-        for i in range(length):
-            tempList.append(random.choice("".join([template])))
-        return "".join(tempList)
+
+length = int(input('Strand Length: '))
+nucleotides ={'A' : 'T', 'T' : 'A', 'C' : 'G', 'G' : 'C'}
+def randStr(length):
+    template = "ATGC"
+    tempList = []
+    for i in range(length):
+        tempList.append(random.choice("".join([template])))
+    return "".join(tempList)
    
-    randomStrand = randStr(length)
+randomStrand = randStr(length)
+print(randomStrand)
     
     #next entry
-        def dnaRep():
-        nontempStrand = ""
-        for x in randomStrand:
+def dnaRep():
+    nontempStrand = ""
+    for x in randomStrand:
+        for key, value in nucleotides.items():
+            if x == key:
+                nontempStrand += value
+    return nontempStrand
+
+print(dnaRep())
+    
+def dnaTranscription():
+    mRNA = ""
+    x = 0
+    for x in randomStrand:
+        if x == 'A':
+            mRNA += 'U'
+        else:
             for key, value in nucleotides.items():
                 if x == key:
-                    nontempStrand += value
-        return nontempStrand
+                    mRNA += value
+    return mRNA
+print(dnaTranscription())
     
-    def dnaTranscription():
-        mRNA = ""
-        x = 0
-        for x in randomStrand:
-            if x == 'A':
-                mRNA += 'U'
-            else:
-                for key, value in nucleotides.items():
-                    if x == key:
-                        mRNA += value
-        return mRNA
-print(dnaTools)
+    
+
     
